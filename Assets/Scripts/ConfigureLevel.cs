3d3b86c74b8324e8885a22648b24832adad5897c
madnanturudu <adnan_turudu@hotmail.com>
2023-03-10 01:22:01 +0300

    Materials and prefabs edited
    Materials outline color yellow,
    Prefabs has foodpack layer

--- Assets/Scripts/ConfigureLevel.cs
+++ Assets/Scripts/ConfigureLevel.cs
@@ -27,9 +27,6 @@ public class ConfigureLevel : MonoSingleton<ConfigureLevel>
         configLevelDelegate -= ConfigSettings;
         configLevelDelegate -= ConfigLabels;
     }
-    private void Start()
-    {
-    }
     public void ConfigLevel(int _level)
     {
         level = _level;
--- Assets/Scripts/Foodpack/FoodpackObj.cs
+++ Assets/Scripts/Foodpack/FoodpackObj.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Rigidbody))]
-public class FoodpackObj : MonoBehaviour
+public class FoodpackObj : MonoBehaviour, IPointerDownHandler
 {
-
+    [SerializeField] MeshRenderer mRenderer;
+    private void Start()
+    {
+        mRenderer = GetComponent<MeshRenderer>(); 
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        mRenderer.material.SetFloat("_Outline", 1);
+        transform.localScale = transform.localScale * 1.3f;
+    }
 }
